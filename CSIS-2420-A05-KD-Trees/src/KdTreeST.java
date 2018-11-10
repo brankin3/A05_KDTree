58dff5c42e48f63921a88fabd92a567c947788b8
NeedACleverName <43866214+NeedACleverName@users.noreply.github.com>
2018-11-10 02:05:57 -0700

    Add files via upload
    Finished PointST and KdTreeST (with commented code). Added test results to the readme.

--- CSIS-2420-A05-KD-Trees/src/KdTreeST.java
+++ CSIS-2420-A05-KD-Trees/src/KdTreeST.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.RectHV;
@@ -6,239 +9,351 @@ public class KdTreeST<Value>
 {
 	private static final boolean X_AXIS = true;
 	private static final boolean Y_AXIS = false;
-	private Node start;
-	int size = 0;
+	private Node start; //The beginning node of the Kd Tree
+	int size = 0; //The current size of the Kd Tree
 	
-	public KdTreeST()
+	public KdTreeST() // construct an empty Kd Tree
 	{
 	}
 	
-	public boolean isEmpty()                      // is the symbol table empty? 
+	 // is the symbol table empty? 
+	public boolean isEmpty()                     
 	{
 		return start == null;
 	}
-	   
-	public int size()                         // number of points 
+	
+	// number of points 
+	public int size()                         
 	{
 		return size;
 	}
-	   
-	public void put(Point2D p, Value val)      // associate the value val with point p
+	
+	 // associate the value val with point p
+	public void put(Point2D p, Value val)     
 	{
-		if (start == null)
+		if (start == null) //Base case: tree is empty
 		{
-			start = new Node(p, val, X_AXIS, null);
-			size++;
+			start = new Node(p, val, X_AXIS); //Set start to a new node with the specified values
+			size++; //Increment size
 		}
 		else
 		{
-			put(p, val, start);
+			put(p, val, start); //Start recursion
 		}
 	}
 	
 	private void put(Point2D p, Value val, Node n)
 	{
-		if (n.p.x() == p.x() && n.p.y() == p.y())
+		if (n.p.x() == p.x() && n.p.y() == p.y()) //Base case: point is already in the table
 		{
-			n.val = val;
+			n.val = val; //Update the value of the current point
 		}
 		
-		else if (n.axisComparsion == X_AXIS)
+		else if (n.axisComparsion == X_AXIS) //Check if current node uses X axis 
 		{
-			if (n.p.x() > p.x())
+			if (n.p.x() > p.x()) //If specified point is less than the current node
 			{
-				if (n.lesser != null)
-					put(p, val, n.lesser);
+				if (n.lesser != null)      //If current node's lesser child is filled
+					put(p, val, n.lesser); //Check lesser child subtree
 				else
 				{
-					n.lesser = new Node(p, val, Y_AXIS, n);
-					size++;
+					n.lesser = new Node(p, val, Y_AXIS); //Set lesser child to a new node with specified values
+					size++; //Increment size
 				}
 			}
-			else 
+			else //Else specified point is greater than or equal to the current node
 			{
-				if (n.greater != null)
-					put(p, val, n.greater);
+				if (n.greater != null)      //If current node's greater child is filled
+					put(p, val, n.greater); //Check greater child subtree
 				else
 				{
-					n.greater = new Node(p, val, Y_AXIS, n);
-					size++;
+					n.greater = new Node(p, val, Y_AXIS); //Set greater child to a new node with specified values
+					size++; //Increment size
 				}
 			}
 		}
 		
-		else if (n.axisComparsion == Y_AXIS)
+		else if (n.axisComparsion == Y_AXIS) //Check if current node uses Y axis 
 		{
-			if (n.p.y() > p.y())
+			if (n.p.y() > p.y()) //If specified point is less than the current node
 			{
-				if (n.lesser != null)
-					put(p, val, n.lesser);
+				if (n.lesser != null)      //If current node's lesser child is filled
+					put(p, val, n.lesser); //Check lesser child subtree
 				else
 				{
-					n.lesser = new Node(p, val, X_AXIS, n);
-					size++;
+					n.lesser = new Node(p, val, X_AXIS); //Set lesser child to a new node with specified values
+					size++; //Increment size
 				}
 			}
-			else 
+			else //Else specified point is greater than or equal to the current node
 			{
-				if (n.greater != null)
-					put(p, val, n.greater);
+				if (n.greater != null)      //If current node's greater child is filled
+					put(p, val, n.greater); //Check greater child subtree
 				else
 				{
-					n.greater = new Node(p, val, X_AXIS, n);
-					size++;
+					n.greater = new Node(p, val, X_AXIS); //Set greater child to a new node with specified values
+					size++; //Increment size
 				}
 			}
 		}
 	}
-	   
-	public Value get(Point2D p)                 // value associated with point p 
+	
+	// value associated with point p 
+	public Value get(Point2D p)                 
 	{
-		return get(p, start);
+		return get(p, start); //Start recursion
 	}
 	
 	public Value get(Point2D p, Node n)
 	{
-		if (n == null)
+		if (n == null) //Base case: no match found
 		{
 			return null;
 		}
 
-		if (n.p.x() == p.x() && n.p.y() == p.y())
+		if (n.p.x() == p.x() && n.p.y() == p.y()) //Base case: match found
 		{
-			return n.val;
+			return n.val; //return the value of the current node
 		}
 
-		if (n.axisComparsion == X_AXIS)
+		if (n.axisComparsion == X_AXIS) //Check if current node uses X axis 
 		{
-			if (n.p.x() > p.x())
+			if (n.p.x() > p.x()) //If specified point is less than the current node
 			{
-				return get(p, n.lesser);
+				return get(p, n.lesser); //Check current node's lesser subtree 
 			}
 			else 
 			{
-				return get(p, n.greater);
+				return get(p, n.greater); //Check current node's greater subtree 
 			}
 		}
 		
-		if (n.axisComparsion == Y_AXIS)
+		if (n.axisComparsion == Y_AXIS) //Check if current node uses Y axis 
 		{
-			if (n.p.y() > p.y())
+			if (n.p.y() > p.y()) //If specified point is less than the current node
 			{
-				return get(p, n.lesser);
+				return get(p, n.lesser); //Check current node's lesser subtree 
 			}
 			else 
 			{
-				return get(p, n.greater);
+				return get(p, n.greater); //Check current node's greater subtree 
 			}
 		}
 		
 		return null;
 	}
 	
-	   
-	public boolean contains(Point2D p)            // does the symbol table contain point p? 
+	// does the symbol table contain point p?   
+	public boolean contains(Point2D p)            
 	{
-		return contains(p, start);
+		return contains(p, start); //Start recursion
 	}
 	
 	private boolean contains(Point2D p, Node n)
 	{
-		if (n == null)
+		if (n == null) //Base case: no match found
 		{
 			return false;
 		}
 
-		if (n.p.x() == p.x() && n.p.y() == p.y())
+		if (n.p.x() == p.x() && n.p.y() == p.y()) //Base case: match found
 		{
 			return true;
 		}
 
-		if (n.axisComparsion == X_AXIS)
+		if (n.axisComparsion == X_AXIS) //Check if current node uses X axis 
 		{
-			if (n.p.x() > p.x())
+			if (n.p.x() > p.x()) //If specified point is less than the current node
 			{
-				return contains(p, n.lesser);
+				return contains(p, n.lesser); //Check current node's lesser subtree 
 			}
 			else 
 			{
-				return contains(p, n.greater);
+				return contains(p, n.greater); //Check current node's greater subtree 
 			}
 		}
 		
-		if (n.axisComparsion == Y_AXIS)
+		if (n.axisComparsion == Y_AXIS) //Check if current node uses Y axis 
 		{
-			if (n.p.y() > p.y())
+			if (n.p.y() > p.y()) //If specified point is less than the current node
 			{
-				return contains(p, n.lesser);
+				return contains(p, n.lesser); //Check current node's lesser subtree 
 			}
 			else 
 			{
-				return contains(p, n.greater);
+				return contains(p, n.greater); //Check current node's greater subtree 
 			}
 		}
 		
 		return false;
 	}
-	   
-	public Iterable<Point2D> points()                       // all points in the symbol table 
+	
+	// all points in the symbol table 
+	public Iterable<Point2D> points()                       
 	{
-		Queue<Point2D> q = new Queue<Point2D>();
-		points(start, q);
-		return q;
+		Queue<Point2D> q = new Queue<Point2D>(); //Initialize queue
+		points(start, q); //Start recursion
+		return q; //Return queue
 	}
 	
 	private void points(Node n, Queue<Point2D> q)
 	{
-		if (n != null)
-		{
-			points(n.lesser, q);
-			points(n.greater, q);
-			q.enqueue(n.p);
+		if (n != null) //Base case: current node is null
+		{ 
+			//Else
+			points(n.lesser, q);  //Check current node's lesser subtree
+			points(n.greater, q); //Check current node's greater subtree
+			q.enqueue(n.p); //Add the current node's point to the queue
 		}
 	}
 	   
 	public Iterable<Point2D> range(RectHV rect)             // all points that are inside the rectangle 
 	{
-		return null;
+		Queue<Point2D> q = new Queue<Point2D>();  //Initialize queue
+		range(rect, start, q); //Start recursion
+		return q; //Return queue
+ 	}
+	
+	private void range(RectHV rect, Node n,  Queue<Point2D> q)             
+	{
+		if (n != null) //Base case: current node is null
+		{ 
+			//Else
+			if (n.axisComparsion == X_AXIS) //Check if current node uses X axis 
+			{
+				if (n.p.x() > rect.xmin()) //Check if specified rectangle intersects with the rectangle left of the current node
+				{
+					range(rect, n.lesser , q); //Check lesser subtree
+				}
+				if (n.p.x() <= rect.xmax()) //Check if specified rectangle intersects with the rectangle right of the current node
+				{
+					range(rect, n.greater, q); //Check greater subtree
+				}
+				if (rect.contains(n.p)) //Check if current node is within the rectangle
+				{
+					q.enqueue(n.p); //Add the current node's point to the queue
+				}
+			}
+			if (n.axisComparsion == Y_AXIS) //Check if current node uses Y axis 
+			{
+				if (n.p.y() > rect.ymin()) //Check if specified rectangle intersects with the rectangle above the current node
+				{
+					range(rect, n.lesser , q); //Check lesser subtree
+				}
+				if (n.p.y() <= rect.ymax()) //Check if specified rectangle intersects with the rectangle below the current node
+				{
+					range(rect, n.greater, q); //Check greater subtree
+				}
+				if (rect.contains(n.p)) //Check if current node is within the rectangle
+				{
+					q.enqueue(n.p); //Add the current node's point to the queue
+				}
+			}
+		}
  	}
 	   
 	public Point2D nearest(Point2D p) 
 	{
-		return null;
+		if (start != null) //Check if the table is not empty
+		{ 
+			return nearest(p, start.p, start); //Start recursion
+		}
+		else
+			return null;
 	}
 	
+	public Point2D nearest(Point2D p, Point2D near, Node n) 
+	{
+		if (n != null) //Base case: current node is null
+		{
+			if (n.axisComparsion == X_AXIS) //Check if current node uses X axis 
+			{
+				if (n.p.x() > p.x()) //If specified point is less than the current node
+				{
+					near = nearest(p, near, n.lesser); //Check lesser subtree for the nearest point
+					                                                                //If the distance to rectangle right of the current node 
+					if (Math.pow((n.p.x() - p.x()), 2) < near.distanceSquaredTo(p)) //less than distance to the current nearest point
+						near = nearest(p, near, n.greater); //Check the greater subtree for the nearest point                     
+				}                                                                                  
+				else 
+				{
+					near = nearest(p, near, n.greater); //Check the greater subtree for the nearest point      
+					                                                                //If the distance to rectangle left of the current node 
+					if (Math.pow((n.p.x() - p.x()), 2) < near.distanceSquaredTo(p)) //less than distance to the current nearest point
+						near = nearest(p, near, n.lesser); //Check the lesser subtree for the nearest point  
+				}
+			}
+			else if (n.axisComparsion == Y_AXIS) //Check if current node uses Y axis 
+			{
+				if (n.p.y() > p.y()) //If specified point is less than the current node
+				{
+					near = nearest(p, near, n.lesser); //Check lesser subtree for the nearest point
+					                                                                //If the distance to rectangle above the current node 
+					if (Math.pow((n.p.y() - p.y()), 2) < near.distanceSquaredTo(p)) //less than distance to the current nearest point
+						near = nearest(p, near, n.greater); //Check the greater subtree for the nearest point  
+				}
+				else 
+				{
+					near = nearest(p, near, n.greater); //Check the greater subtree for the nearest point  
+					                                                                //If the distance to rectangle below the current node 
+					if (Math.pow((n.p.y() - p.y()), 2) < near.distanceSquaredTo(p)) //less than distance to the current nearest point
+						near = nearest(p, near, n.lesser); //Check lesser subtree for the nearest point
+				}
+			}
+			
+			if (p.distanceSquaredTo(n.p) < p.distanceSquaredTo(near)) //Check if the distance to the current node is less than the distance to the current nearest point
+			{
+				return n.p; //Return the current node's point
+			}
+		}
+		
+		return near; //Return current nearest point
+	}
+	
+	//Node class to build the Kd Tree
 	private class Node
 	{
-		public Point2D p;
-		public Value val;
-		public boolean axisComparsion;
-		public Node parent;
-		public Node greater;
-		public Node lesser;
+		public Point2D p; //The key 
+		public Value val; //The assigned value
+		public boolean axisComparsion; //Determines which axis to be compared 
+		public Node greater; //Reference to a node with a greater key value
+		public Node lesser; //Reference to a node with a lesser key value
 		
-		public Node(Point2D p, Value val, boolean b, Node par)
+		public Node(Point2D p, Value val, boolean b) //Constructor
 		{
 			this.p = p;
 			this.val = val;
 			this.axisComparsion = b;
-			this.parent = par;
 		}
 	}
 	
 	public static void main(String[] args)
 	{
-		KdTreeST<Integer> st = new KdTreeST<Integer>();
-		st.put(new Point2D(0.0,0.0), 5);
-		st.put(new Point2D(1.1,1.1), 1);
-		st.put(new Point2D(-2.2,-2.2), 2);
-		st.put(new Point2D(3.3,3.3), 3);
-		st.put(new Point2D(3.3,3.3), 333);
-		st.put(new Point2D(4.4,4.4), 4);
-		
-		System.out.println(st.get(new Point2D(3.3,3.3)));
-		System.out.println(st.contains(new Point2D(-2.2,-2.2)));
-		System.out.println(st.points());
-
+		String filename = "C:\\TestDocs\\input1M.txt";
+        In in = new In(filename);
+        
+        KdTreeST<Integer> st = new KdTreeST<Integer>();
+        for (int i = 0; !in.isEmpty(); i++) 
+        {
+        	double x = in.readDouble();
+        	double y = in.readDouble();
+        	Point2D p = new Point2D(x, y);
+        	st.put(p, i);
+        }
+        Random r = new Random();
+        
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 5000000; i++)
+        {
+        	Point2D p = new Point2D(r.nextDouble(), r.nextDouble());
+        	st.nearest(p);
+//        	System.out.println(st.nearest(p));
+        }
+        long fin = System.currentTimeMillis();
+        double seconds = (fin - start)/ 1000.0;
+        double calls = 5000000/seconds;
+        
+        System.out.println("Time: " + seconds);
+        System.out.println("Calls per second: " + calls);
 	}
 }
--- CSIS-2420-A05-KD-Trees/src/PointST.java
+++ CSIS-2420-A05-KD-Trees/src/PointST.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.RectHV;
@@ -5,75 +8,84 @@ import edu.princeton.cs.algs4.RedBlackBST;
 
 public class PointST<Value>
 {
-	private RedBlackBST<Point2D, Value> st;
+	private RedBlackBST<Point2D, Value> st; //Red Black BST to hold 2D point and values
 	
-	public PointST()                                // construct an empty symbol table of points 
+	 // construct an empty symbol table of points
+	public PointST()                                
 	{
 		st = new RedBlackBST<Point2D, Value>();
 	}
-	   
-	public boolean isEmpty()                      // is the symbol table empty? 
+	
+	// is the symbol table empty?
+	public boolean isEmpty()                       
 	{
 		return st.isEmpty();
 	}
-	   
-	public int size()                         // number of points 
+	
+	 // number of points 
+	public int size()                          
 	{
 		return st.size();
 	}
-	   
-	public void put(Point2D p, Value val)      // associate the value val with point p
+	
+	 // associate the value val with point p
+	public void put(Point2D p, Value val)     
 	{
 		st.put(p, val);
 	}
-	   
-	public Value get(Point2D p)                 // value associated with point p 
+	
+	// value associated with point p 
+	public Value get(Point2D p)                 
 	{
 		return st.get(p);
 	}
-	   
-	public boolean contains(Point2D p)            // does the symbol table contain point p? 
+	 
+	// does the symbol table contain point p? 
+	public boolean contains(Point2D p)            
 	{
 		return st.contains(p);
 	}
 	   
-	public Iterable<Point2D> points()                       // all points in the symbol table 
+	// all points in the symbol table 
+	public Iterable<Point2D> points()                        
 	{
 		return st.keys();
 	}
-	   
-	public Iterable<Point2D> range(RectHV rect)             // all points that are inside the rectangle 
+	 
+	// all points that are inside the rectangle 
+	public Iterable<Point2D> range(RectHV rect)             
 	{
 		Queue<Point2D> q = new Queue<Point2D>();
 		
-		for (Point2D p : st.keys())
+		for (Point2D p : st.keys()) //Iterate through all points in the table
 		{
-			if (rect.contains(p))
+			if (rect.contains(p)) //If the current point is in the rectangle
 			{
-				q.enqueue(p);
+				q.enqueue(p); //Add the current point to the queue
 			}
 		}
 		
 		return q;
  	}
-	   
+	
+	// a nearest neighbor to point p; null if the symbol table is empty 
 	public Point2D nearest(Point2D p) 
 	{
 		Point2D nearest = null;
 		Double n = null;
 		
-		for (Point2D s : st.keys())
+		for (Point2D s : st.keys()) //Iterate through all points in the table
 		{
-			if (n == null)
+			if (n == null) //If nearest has not been determined
 			{
-				n = p.distanceSquaredTo(s);
-				nearest = s;
+				n = p.distanceSquaredTo(s); //Set n to the current distance
+				nearest = s; //Set nearest to the current point
 			}
 			
-			if (n > p.distanceSquaredTo(s))
+			if (n > p.distanceSquaredTo(s)) //If n is greater than the current distance
 			{
-				n = p.distanceSquaredTo(s);
-				nearest = s;
+				n = p.distanceSquaredTo(s); //Set n to the current distance
+				nearest = s; //Set nearest to the current point
 			}
 		}
 		
@@ -82,8 +94,35 @@ public class PointST<Value>
 	
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-
+		String filename = "C:\\TestDocs\\input1M.txt";
+        In in = new In(filename);
+        
+        PointST<Integer> st = new PointST<Integer>();
+        for (int i = 0; !in.isEmpty(); i++) 
+        {
+        	double x = in.readDouble();
+        	double y = in.readDouble();
+        	Point2D p = new Point2D(x, y);
+        	st.put(p, i);
+        }
+        Random r = new Random();
+        
+        int calls = 5000;
+        
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < calls; i++)
+        {
+        	Point2D p = new Point2D(r.nextDouble(), r.nextDouble());
+        	st.nearest(p);
+//        	System.out.println(st.nearest(p));
+        }
+        long fin = System.currentTimeMillis();
+        double seconds = (fin - start)/ 1000.0;
+        double results = calls/seconds;
+        
+        System.out.println("Total number of calls: " + calls);
+        System.out.println("Time: " + seconds);
+        System.out.println("Calls per second: " + results);
 	}
 
 }
